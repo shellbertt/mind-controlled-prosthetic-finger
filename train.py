a2ab9ec524ed202bdc70f358386bb136dbc0672f
Kaaden RumanCam <kaaden@ualberta.ca>
2024-11-16 17:51:02 -0700

    train multiple times to see mean score

--- train.py
+++ train.py
@@ -1,18 +1,18 @@
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import fetch_openml
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import *
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.utils import check_random_state
 
-# Turn down for faster convergence
-t0 = time.time()
 
 # Load data from https://www.openml.org/d/554
-X = np.load("X.npy")
-y = np.load("y.npy")
+suffix = "2"
+X = np.load(f"backup/X{suffix}.npy")
+# base = 3 * 200
+# X = X[:,base:base+200]
+y = np.load(f"backup/y{suffix}.npy")
 print("X")
 print(type(X))
 print(X.shape)
@@ -21,46 +21,36 @@ print(type(y))
 print(y.shape)
 train_samples = len(y)
 
-random_state = check_random_state(0)
-permutation = random_state.permutation(X.shape[0])
-X = X[permutation]
-y = y[permutation]
-X = X.reshape((X.shape[0], -1))
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, train_size=None, test_size=None
-)
-
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
 # Turn up tolerance for faster convergence
-clf = LogisticRegression(C=50.0 / train_samples, penalty="l1", solver="saga", tol=0.1)
-clf.fit(X_train, y_train)
-sparsity = np.mean(clf.coef_ == 0) * 100
-score = clf.score(X_test, y_test)
-# print('Best C % .4f' % clf.C_)
-print("Sparsity with L1 penalty: %.2f%%" % sparsity)
-print("Test score with L1 penalty: %.4f" % score)
+clf = LogisticRegression(C=50.0 / train_samples, penalty="l2", solver="liblinear", tol=0.01)
 
-coef = clf.coef_.copy()
-plt.figure(figsize=(10, 5))
-scale = np.abs(coef).max()
-for i in range(10):
-    l1_plot = plt.subplot(2, 5, i + 1)
-    l1_plot.imshow(
-        coef[i].reshape(4, 200),
-        interpolation="nearest",
-        cmap=plt.cm.RdBu,
-        vmin=-scale,
-        vmax=scale,
+def evaluate(clf):
+    global X, y
+
+    t0 = time.time()
+
+    random_state = check_random_state(0)
+    permutation = random_state.permutation(X.shape[0])
+    X = X[permutation]
+    y = y[permutation]
+    X = X.reshape((X.shape[0], -1))
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=None, test_size=None
     )
-    l1_plot.set_xticks(())
-    l1_plot.set_yticks(())
-    l1_plot.set_xlabel("Class %i" % i)
-plt.suptitle("Classification vector for...")
 
-run_time = time.time() - t0
-print("Example run in %.3f s" % run_time)
-plt.show()
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    clf.fit(X_train, y_train)
+    sparsity = np.mean(clf.coef_ == 0) * 100
+    score = clf.score(X_test, y_test)
+#   print('Best C % .4f' % clf.C_)
+    print("Sparsity with L1 penalty: %.2f%%" % sparsity)
+    print("Test score with L1 penalty: %.4f" % score)
+    run_time = time.time() - t0
+    print("Example run in %.3f s" % run_time)
+    return score
+
+print(f"{np.mean([evaluate(clf) for _ in range(100)])=}")
